f665dea5f6b72c0f363d7dd6b098f799fc7ea879
Anthony Fernandez <anthony@pictarine.com>
2020-08-04 18:30:58 +0200

    Update SnipViewApp.swift

--- Snip/SnipViewApp.swift
+++ Snip/SnipViewApp.swift
@@ -18,12 +18,32 @@ struct SnipViewApp: View {
   @ViewBuilder
   var appNavigation: some View {
     NavigationView {
-      Sidebar(viewModel: SideBarViewModel(snippets: SnipItem.preview()))
-        .visualEffect(material: .sidebar)
-        .frame(minWidth: 180, idealWidth: 200, maxWidth: 240)
-      EmptyView()
+      
+      sideBar
+      openingPanel
     }
     .edgesIgnoringSafeArea(.top)
+  }
+  
+  var sideBar: some View {
+    Sidebar(viewModel: SideBarViewModel(snippets: SnipItem.preview()))
+    .visualEffect(material: .sidebar)
+    .frame(minWidth: 180, idealWidth: 200, maxWidth: 240)
+  }
+  
+  var openingPanel: some View {
+    VStack {
+      Spacer()
+      HStack {
+        Spacer()
+        Text("Create your first Snipppet")
+          .font(Font.custom("HelveticaNeue-Light", size: 20))
+        Spacer()
+      }
+      Spacer()
+    }
+    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
+    .background(Color.BLACK_500)
   }
 }
 
