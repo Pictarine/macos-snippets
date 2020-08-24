1f9f3c7dd409c44a63ba1cf3613143a522704526
Anthony Fernandez <anthony@pictarine.com>
2020-08-24 14:17:26 +0200

    Update SnipItemsListAction.swift

--- Snip/Components/Sidebar/SnipItemsListAction.swift
+++ Snip/Components/Sidebar/SnipItemsListAction.swift
@@ -122,20 +122,25 @@ struct SnipItemsListAction {
         snip.lastUpdateDate = Date()
       }
       
-      snip.syncState = .syncing
+      if let gistId = snip.gistId {
+          
+        snip.syncState = .syncing
+        
+        SyncManager.shared.updateGist(id: gistId, title: snip.name, code: snip.snippet)
+          .receive(on: DispatchQueue.main)
+          .sink(receiveCompletion: { (completion) in
+            if case let .failure(error) = completion {
+              print(error)
+            }
+          }, receiveValue: { (gist) in
+            snip.gistId = gist.id
+            snip.gistURL = gist.url
+            snip.syncState = .synced
+          })
+          .store(in: &stores)
+      }
       
-      SyncManager.shared.createGist(title: snip.name, code: snip.snippet)
-        .receive(on: DispatchQueue.main)
-        .sink(receiveCompletion: { (completion) in
-          if case let .failure(error) = completion {
-            print(error)
-          }
-        }, receiveValue: { (gist) in
-          snip.gistId = gist.id
-          snip.gistURL = gist.url
-          snip.syncState = .synced
-        })
-        .store(in: &stores)
+      
     }
   }
   
