c424612d238a919030ebed2910a2f0f14c741e7a
Anthony Fernandez <anthony@pictarine.com>
2020-08-28 11:07:02 +0200

    New window handling

--- Snip/AppDelegate.swift
+++ Snip/AppDelegate.swift
@@ -13,13 +13,29 @@ import Combine
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
   
+  lazy var windows = NSWindow()
   var window: NSWindow!
   
   func applicationDidFinishLaunching(_ aNotification: Notification) {
     
     SyncManager.shared.initialize()
     
-    let contentView = SnipViewApp()
+    createWindow()
+  }
+  
+  func applicationWillTerminate(_ aNotification: Notification) {
+  }
+  
+  func application(_ application: NSApplication, open urls: [URL]) {
+    SyncManager.shared.handleDeepLink(urls: urls)
+  }
+  
+  
+  // Window
+  
+  func createWindow() {
+    
+    let snipAppView = SnipViewApp()
       .environmentObject(Settings())
       .environmentObject(AppState())
       .edgesIgnoringSafeArea(.top)
@@ -35,18 +51,26 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     window.isMovableByWindowBackground = true
     window.center()
     window.setFrameAutosaveName("Main Window")
-    window.contentView = NSHostingView(rootView: contentView)
+    window.contentView = NSHostingView(rootView: snipAppView)
+    window.isReleasedWhenClosed = false
     DispatchQueue.main.async {
       self.window.orderOut(nil)
       self.window.makeKeyAndOrderFront(nil)
     }
   }
   
-  func applicationWillTerminate(_ aNotification: Notification) {
+  func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+    if !flag {
+        sender.windows.forEach { $0.makeKeyAndOrderFront(self) }
+    }
+    return true
   }
   
-  func application(_ application: NSApplication, open urls: [URL]) {
-    SyncManager.shared.handleDeepLink(urls: urls)
+  
+  // Menu
+  
+  @IBAction func newWindow(_ sender: Any) {
+    window.makeKeyAndOrderFront(self)
   }
 }
 
